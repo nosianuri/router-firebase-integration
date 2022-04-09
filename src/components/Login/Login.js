a5e091be4d64f78b9e27b7a61692cf34cf81dbca
nosianuri <nurinosia@gmail.com>
2022-04-09 20:41:58 +0600

    added onauthstate changed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const {singInWidthGoogle} = useFirebase();
     return (
         <div>
             <h3>Please Login</h3>
+            <div style={{margin: '20px'}}>
+            <button onClick={singInWidthGoogle}>Google Sign In</button>
+            </div>
+            <form >  
+            <input type="email" placeholder='Your email' />
+            <br />
+            <input type="password" name="" id="" placeholder='password' />
+            <br />
+                <input type="submit" value="Login" />
+            </form>
+            
         </div>
     );
 };
---
+++ src/components/Register/Register.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3>Please Register now</h3>
+            <form>
+                <input type="text" placeholder='Your name' />
+                <br />
+                <input type="email" placeholder='Your email' />
+                <br />
+                <input type="password" name="" id="" placeholder='password' />
+                <br />
+                <input type="submit" value="Register" />
+            </form>
+
+        </div>
+    );
+};
+
+export default Register;
